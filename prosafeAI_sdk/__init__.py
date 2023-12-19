c6a95d8fbe65162897e8209f1edf6ffe4e897c83
sunchangkai <864640842@qq.com>
2023-12-19 16:13:22 +0800

    Delete prosafeAI_sdk directory

--- prosafeAI_sdk/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-# @Time    : 2023/4/3 下午3:23
-# @Author  : liuliping
-# @File    : __init__.py.py
-# @description:
--- prosafeAI_sdk/robustness_test/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-@Time ： 25/4/2023 2:47 pm
-@Auth ： Jingrui Han
-@File ：__init__.py.py
-@IDE ：PyCharm
-@Motto: ProsafeAI (AI Hub China)
-"""
